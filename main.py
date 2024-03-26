1512a91d4376989031039d03ae28f7074fababfe
YZenia <164417240+YZenia@users.noreply.github.com>
2024-03-26 20:55:36 +0100

    back step

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-pygame.display.set_caption("The game is Til")
-icon = pygame.image.load("")
-
 
 running = True
 while running:
