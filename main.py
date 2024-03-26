b7b4cdb2eff4e57d6c563de2f7defe2a0602acf6
YZenia <164417240+YZenia@users.noreply.github.com>
2024-03-26 21:52:53 +0100

    apply

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("The game is Til")
-icon = pygame.image.load("image/aimLogo.png")
+icon = pygame.image.load("img/aimLogo.png")
 pygame.display.set_icon(icon)
 
-target_image = pygame.image.load("image/target.png")
+target_image = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
